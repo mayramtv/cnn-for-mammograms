6f70ee32f4238977be1f5f4704a063a7f614a783
MayraMTV <81662672+mayramtv@users.noreply.github.com>
2025-07-27 14:22:59 -0400

    add functions structure

--- P2_Preporcessing_Utils.py
+++ P2_Preporcessing_Utils.py
@@ -1,1 +1,19 @@
 
+def image_enhancement():
+    pass
+
+def crop():
+    pass
+
+def noise_reduction():
+    pass
+
+def edge_enhancement():
+    pass
+
+def background_removal():
+    pass
+
+def textural_preporcessing():
+    pass
+
---
+++ P3_Training_Utils.py
@@ -0,0 +1,15 @@
+
+def model_architecture():
+    pass
+
+def custom_CNN_architecture():
+    pass
+
+def VGG_CNN_architecture():
+    pass
+
+def ResNet_CNN_Architecture():
+    pass
+
+
+

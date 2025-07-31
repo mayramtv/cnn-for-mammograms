149f028f789702e7ee2c9eddd3cb4d9701e595a8
MayraMTV <81662672+mayramtv@users.noreply.github.com>
2025-07-31 00:07:08 -0400

    Create functions(model, evaluation, save) for phase 2

--- P2_Preporcessing_Utils.py
+++ P2_Preporcessing_Utils.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
+import os
+from datetime import datetime
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score,  f1_score, precision_score, recall_score, roc_auc_score
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Test
 class Test_py:
@@ -116,8 +121,7 @@ def image_iterator(data_sets, suffle=False):
 
 class Basic_Custom_CNN:
     '''Setup and runs model'''
-    def __init__(self, phase=2, input_shape=(256, 256, 1), num_classes=2, epochs):
-        self.phase = phase
+    def __init__(self, input_shape=(256, 256, 1), num_classes=2, epochs):
         self.input_shape = input_shape
         self.classes = num_classes
         self.epochs = epochs
@@ -148,39 +152,128 @@ class Basic_Custom_CNN:
     
         self.model = model
     
-    def train_model(self):
+    def train_model(self, train_gen, val_gen=None epochs=10):
+        '''Trains model'''
+        # verify that model is initialized
+        if self.model is None:
+            raise ValueError("You need to initialize model using architecture() before training")
+            
         # fit data to model
-        history = model.fit(train_gen, validation_data=val_gen, epochs=10)
-        history
-    
-    def custom_CNN_fit(self):
-        pass
+        model.fit(train_gen, validation_data=val_gen, epochs=epochs)
+        
+    def get_model(self):
+        '''Returns trained model'''
+        return self.model
 
+    def save_model(self, models_directory="Models", model_file):
+        '''Save model '''
+
+        # makes directory for outputs
+        os.makedir(models_directory, exist_ok=True)
+
+        # creates path and saves model
+        path = os.path.join(models_directory, model_file)
+        self.model.save(path)
 
 
 # Models evaluation
 class Evaluation:
-    def __init__(self, model, test_data):
+    '''Make predictions and evaluation of the model and calculates evaluation metrics'''
+    def __init__(self, model):
         self.model = model
-        self.X_test_data = test_data[0]
-        self.y_actual = test_data[1]
+        self.metrics = {}
 
-    def custom_CNN_predict(self):
-        pass 
+    def evaluate(self, test_gen):
+        '''Evaluate model and return metrics defined during model compile'''
+        return self.model.evaluate(test_gen)
+
+    def predict(self, test_gen):
+        '''Makes predictions on unseen data'''
+        return self.model.predict(test_gen) 
 
     def confusion_matrix(self):
-        pass
+        '''Claculate confusion metrics and return:
+        True Negative, False Positive, False Negative and True Positive'''
 
-    def calculate_metrics(self):
-        pass
+        
+
+    def calculate_metrics(self, y_true, y_predics, conf_matrix):
+        '''Claculate confusion matrix (True Negative, False Positive, False Negative and True Positive) 
+        and evaluation metrics (Accuracy, Precision, Recall, F1 score, AUC
+        Specificity, False Positive Rate, and False Negative Rate)'''
+        # find predicted class
+        y_pred_class = (y_predics > 0.5).astype(int)
+
+        # get confusion matrix
+        tn, fp, fn, tp = confusion_matrix(y_true, y_pred_class).ravel()
+        
+        # calculate and save metrics
+        self.metrics["confusion_matrix"] = confusion_matrix(y_true, y_pred_class)
+        self.metrics["accuracy"] = accuracy_score(y_true, y_pred_class)
+        self.metrics["precision"] = precision_score(y_true, y_pred_class) # Positive predicive value: identify correct positives
+        self.metrics["recall"] = recall_score(y_true, y_pred_class)       # sensitivity: identify positives 
+        self.metrics["f1_score"] = f1_score(y_true, y_pred_class)
+        self.metrics["auc"] = roc_auc_score(y_true, y_predics)
+        self.metrics["specificity"] = tn / (tn + fp)     # Identify negatives 
+        self.metrics["fpr"] = fp / (fp + tn)             # identify false positive rate
+        self.metrics["fnr"] = fn / (fn + tp)             # identify false negative rate
+
+
+# Save output data
+class Save_Data:
+    '''Save models data: model information, evaluation metrics, data and comments'''
+    
+    def __init__(self, file_name="models_data.json", out_directory="Outputs"):
+        self.out_directory = out_directory
+        self.file_name = file_name
+        self.path = os.path.join(self.out_directory, self.file_name)
+        
+        # makes directory for outputs
+        os.makedir(out_directory, exist_ok=True)
+
+        # open json file, if exist, to load existing output data
+        if os.path.exist(self.path):
+            with open(self.path, 'r') as file:
+                self.output_data = json.load(file)
+        else:
+            self.output_data = {}
+        
+    def add_model_data(self, model_name, model_path, metrics, project_phase, comments=""):
+        '''Saves model data into a dictionary'''
+        
+        self.output_data[model_name] = {
+            'time_stamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'model_path': model_path,
+            'evaluation_metrics': metrics,
+            'project_phase': project_phase,
+            'comments': comments
+        }
+        
+        
+    def save_model_data(self):
+        '''Saves data dictionary into json file'''
+        with open(self.path, 'w') as file:
+                json.dump(self.output_data, file, indent=4)
+        print(f"[INFO]Models data is saved to {self.path}")
+        
 
 # Models visualization 
 class Visualization:
-    def __init__(self, results):
-        pass
+    def __init__(self, file_name="models_data.json", out_directory="Outputs"):
+        self.out_directory = out_directory
+        self.file_name = file_name
+        self.path = os.path.join(self.out_directory, self.file_name)
+        self.models_data = None
 
-    def confusion_matrix(self):
-        pass
+        # open json file to load existing output data
+        if os.path.exist(self.path):
+            with open(self.path, 'r') as file:
+                self.models_data = json.load(file)
+        else:
+            print("No models are saved")
+
+    def confusion_matrix(self, model_name):
+        
 
     def line_plot(self):
         pass
@@ -193,24 +286,5 @@ class Visualization:
 
     def ROC_curve(self):
         pass
-
-
-# Save output data
-class Save_Data:
-    def __init__(self, model=None, model_name="model", save_directory="Outputs"):
-        self.model = model
-        self.model_name = model_name
-        self.save_directory = save_directory
-        self.data = {} 
-
-    def add_metric(self, metric, value):
-        pass
-        
-    def add_description(self, description):
-        pass
-
-    def save_data(self, filename=None):
-        pass
-    
 
 
